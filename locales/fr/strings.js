6553cb2298ae7741bbebe94eb53d139e0f50ce2a
Christoph <Christoph142@myopera.com>
2013-03-13 10:55:39 +0100

    French translation

--- locales/fr/strings.js
+++ locales/fr/strings.js
@@ -1,14 +1,14 @@
 var strings = {
 	"lang" : "fr",
 	"by" : "de",
-	"textarea" : "Multiline textareas:",
-	"resizable" : "To which extend do you want automatically adjusted textareas to be manually adjustable?",
-	"unchanged" : "unchanged",
-	"both" : "in both directions",
-	"horizontal" : "horizontal only",
-	"vertical" : "vertical only",
-	"none" : "not at all",
-	"textarea and input" : "All kinds of text inputs:",
-	"transition-duration" : "Transition-duration of height adjustments",
+	"textarea" : "Zones de texte multilignes:",
+	"resizable" : "Comment veux-tu les zones de texte ajustés automatiquement d'être réglable manuellement?",
+	"unchanged" : "inchangé",
+	"both" : "aux deux directions",
+	"horizontal" : "seulement horizontale",
+	"vertical" : "seulement verticale",
+	"none" : "pas du tout",
+	"textarea and input" : "Tous les types de saisies de texte:",
+	"transition-duration" : "Durée de la transition du réglage de la hauteur",
 	"footer" : "Merci d'avoir installé cette extension. Tu peux me contacter toujours pour des commentaires et des questions."
 };
