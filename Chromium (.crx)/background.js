436569593347516073af076d8e2dc73f481e0c5b
Christoph142 <christoph142@gmx.com>
2014-02-24 13:18:52 +0100

    convert into event page

--- Chromium (.crx)/background.js
+++ Chromium (.crx)/background.js
@@ -1,14 +1,13 @@
 //retrieve and store settings (filled with default values) for all pages:
-function update_settings(){ chrome.storage.sync.get( null, function(storage){
+var w = null;
+chrome.storage.sync.get( null, function (storage){
 	w = {
 	"resizable" :			(!storage["resizable"]				? "horizontal" 	: storage["resizable"]),
 	"transition_duration" :	(!storage["transition_duration"]	? "300" 		: storage["transition_duration"]),
 	"collapse_textareas" :	(!storage["collapse_textareas"]		? "0" 			: storage["collapse_textareas"])
 	};
-}); }
-update_settings();
+});
 
 chrome.extension.onMessage.addListener( function(request, sender, sendResponse){
-	if		(request.data === "settings")						sendResponse(w);
-	else if (request.data === "update_settings")				update_settings(); // will request options page update when finished
+	if (request.data === "settings") sendResponse(w);
 });
